ba76a073c7827cf0906b16d8f2a1b19e27919395
elim655 <51264790+elim655@users.noreply.github.com>
2024-03-06 10:25:14 -0600

    .

--- Midterm/index.js
+++ Midterm/index.js
@@ -5,6 +5,7 @@ fetch("./data.json")
     function loadArtists(myArtists){
         var CardArtists = document.getElementById("col");
         for(var i=0; i<myArtists.artists.length; i++){
+            let card = "card-body" + i.toString();
             let name = myArtists.artists[i].artist_name;
             let genre = myArtists.artists[i].genre_of_music;
             let url = myArtists.artists[i].picture;
@@ -14,14 +15,28 @@ fetch("./data.json")
             AddCardArtist.classList.add("col");
             AddCardArtist.innerHTML = `
             <div class="card shadow-sm">
-                <h3 class="text-center"><strong>${name}</strong></h3>
+                <h2 class="text-center"><strong>${name}</strong></h2>
+                <h4 class="text-center">${genre}</h4>
                 <img src=${url} class="card-img-top" alt="${name}" ></img>
-                <div class="card-body">
-                  <p class="card-text">${genre}</p>
-                  <div class="d-flex justify-content-between align-items-center">
-                  </div>
+                <div id=${card} class="card-body">
+
                 </div>
               </div>`;
               CardArtists.appendChild(AddCardArtist);
+
+              for(var j=0; j< event.length; j++){
+                var eventArtist = document.getElementById(card);
+                let date = event[j].date;
+                let time = event[j].time;
+                let state = event[j].state;
+                let city = event[j].city;
+                let building = event[j].building;
+
+                let AddEvent = document.createElement("div");
+                AddEvent.innerHTML = `
+                <p class="card-text"><strong>${date}</strong>, ${time} at ${building} ${city}, ${state}</p>
+                `;
+                eventArtist.appendChild(AddEvent);
+              }
         }
     }
